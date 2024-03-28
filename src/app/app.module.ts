5705c5ff2219517626b6416148ab507aaedc7283
Yusuf Zeyrek <zeyrek.yusuf3405@gmail.com>
2024-03-28 23:42:50 +0300

    end of lesson 156

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,11 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CardStatusComponent } from './components/card-status/card-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
+  {path : 'checkout', component : CheckoutComponent},
   {path : 'cart-details', component : CartDetailsComponent},
   {path : 'products/:id', component : ProductDetailsComponent},
   {path : 'search/:keyword', component : ProductListComponent},
@@ -35,14 +38,16 @@ const routes: Routes = [
     SearchComponent,
     ProductDetailsComponent,
     CardStatusComponent,
-    CartDetailsComponent
+    CartDetailsComponent,
+    CheckoutComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
     //ng - bootstrap
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
