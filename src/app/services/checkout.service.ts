57f9e69f54d43c772ceefdd49e8ba434cfafaed2
Yusuf Zeyrek <zeyrek.yusuf3405@gmail.com>
2024-03-30 16:52:03 +0300

    bug fix

--- src/app/services/checkout.service.ts
+++ src/app/services/checkout.service.ts
@@ -13,7 +13,8 @@ export class CheckoutService {
   constructor(private httpClient: HttpClient) { }
 
   placeOrder(purchase: Purchase) : Observable<any> {
-    return this.httpClient.post<Purchase>(this.purchaseUrl, this.purchaseUrl);
+    debugger;
+    return this.httpClient.post<Purchase>(this.purchaseUrl, purchase);
     
   }
 }
