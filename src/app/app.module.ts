ecc385c8d0f05dd19fd74eb0650c708cbb0bf1f7
Yusuf Zeyrek <zeyrek.yusuf3405@gmail.com>
2024-03-30 23:50:17 +0300

    end of lesson 238

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http'
 import { ProductService } from './services/product.service';
-import {Routes, RouterModule} from '@angular/router';
+import {Routes, RouterModule, Router} from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component'
@@ -15,8 +15,39 @@ import { CardStatusComponent } from './components/card-status/card-status.compon
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
+import { LoginStatusComponent } from './components/login-status/login-status.component';
+import { from } from 'rxjs';
+
+import{
+  OktaAuthModule,
+  OktaCallbackComponent,
+  OKTA_CONFIG,
+  OktaAuthGuard
+} from'@okta/okta-angular'
+
+import { OktaAuth } from '@okta/okta-auth-js';
+import myAppConfig from './config/my-app-config';
+import { MembersPageComponent } from './components/members-page/members-page.component';
+
+const oktaConfig = myAppConfig.oidc;
+const oktaAuth = new OktaAuth(oktaConfig);
+
+function sendToLoginPage(oktaAuth : OktaAuth, injector: Injector) {
+  // use injector to access any service available within your app.
+  const router = injector.get(Router);
+
+  // redirect the user to your custom login page
+  router.navigate(['/login']);
+}
 
 const routes: Routes = [
+  {path : 'members', component : MembersPageComponent, canActivate: [OktaAuthGuard],
+                    data: {onAuthRequired : sendToLoginPage}},
+
+  {path : 'login/callback', component : OktaCallbackComponent},
+  {path : 'login', component : LoginComponent},
+
   {path : 'checkout', component : CheckoutComponent},
   {path : 'cart-details', component : CartDetailsComponent},
   {path : 'products/:id', component : ProductDetailsComponent},
@@ -27,8 +58,7 @@ const routes: Routes = [
   {path : '', redirectTo: '/products', pathMatch : 'full'},
   {path : '**', redirectTo : '/products', pathMatch : 'full'},
 
-
-]
+];
 
 @NgModule({
   declarations: [
@@ -39,7 +69,10 @@ const routes: Routes = [
     ProductDetailsComponent,
     CardStatusComponent,
     CartDetailsComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    LoginComponent,
+    LoginStatusComponent,
+    MembersPageComponent,
   ],
   imports: [
     RouterModule.forRoot(routes),
@@ -47,9 +80,10 @@ const routes: Routes = [
     HttpClientModule,
     //ng - bootstrap
     NgbModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    OktaAuthModule
   ],
-  providers: [ProductService],
+  providers: [ProductService, {provide: OKTA_CONFIG, useValue: {oktaAuth}}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,11 @@
+export default {
+    oidc: {
+        clientId: '0oag4q3b64CQiWaPt5d7',
+        issuer: 'https://dev-69362782.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid','profile','email']
+    }
+
+}
+
+
