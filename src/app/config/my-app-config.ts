a1944ca5fcb62a92382a133bcc8ecfa13c9150c0
Yusuf Zeyrek <zeyrek.yusuf3405@gmail.com>
2024-03-31 19:10:34 +0300

    end of 280

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc: {
         clientId: '0oag4q3b64CQiWaPt5d7',
         issuer: 'https://dev-69362782.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid','profile','email']
     }
 
